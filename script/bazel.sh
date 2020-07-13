858e6f0bbc8ba9750fecb581d7ffae23f4a4a039
michael1017 <michael881017@gmail.com>
2020-07-13 18:34:05 +0800

    modify bazel

--- script/bazel.sh
+++ script/bazel.sh
@@ -1,8 +1,9 @@
+#/bin/bash -e
 VERSION=$1
 cd $HOME/toolchains
-wget https://github.com/bazelbuild/bazel/archive/$VERSION.zip
-unzip $VERSION.zip
-cd bazel-$VERSION
+wget https://github.com/bazelbuild/bazel/releases/download/$VERSION/bazel-$VERSION-dist.zip
+unzip bazel-$VERSION-dist.zip -d bazel-$VERSION-dist
+cd bazel-$VERSION-dist
 env EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk" bash ./compile.sh
 echo "Remember to add bazel in your PATH\n"
 echo "Bazel is in bazel/output"
